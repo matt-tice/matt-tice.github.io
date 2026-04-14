2ee39373e6d24d1177a9c9fbaeba6e868d3e0574
matt-tice <76890688+matt-tice@users.noreply.github.com>
2026-04-14 13:26:23 -0400

    Changed to getElementById

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,8 +273,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const targetId = btn.getAttribute('data-target');
     const textToCopy = document.getElementById(targetId)?.innerText;
-    const copyIcon = btn.querySelector('.copy-icon');
-    const checkIcon = btn.querySelector('.check-icon');
+    const copyIcon = btn.getElementById('copy-icon');
+    const checkIcon = btn.getElementById('check-icon');
 
     if (!textToCopy) return;
 
